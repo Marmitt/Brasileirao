d83016c7e1d8bc33d0827cdfc2c1fa33b7767537
Mateus Marmitt <lmateus.marmitt@gmail.com>
2017-10-31 21:12:46 -0200

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Brasileirao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
