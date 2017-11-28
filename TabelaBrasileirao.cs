5cd6fead574408d577f2216904b910715bf6d141
Marmitt <lmateus.marmitt@gmail.com>
2017-11-28 21:17:56 -0200

    Resolvendo problema do campeao

--- TabelaBrasileirao.cs
+++ TabelaBrasileirao.cs
@@ -7,19 +7,23 @@ namespace Brasileirao
     class TabelaBrasileirao
     {
         public List<Times> times;
-        const int quantrebaixados = 4;
+        const int quantreb = 1;
 
         public TabelaBrasileirao() {
             times = new List<Times>();
         }
 
         public void resultado() {
+            string premiacao = "";
             for (int i = 0; i < this.times.Count; i++) {
+                if (i == 0)
+                    premiacao = "= Campeão";
+                if (i >= 2)
+                if (i >= this.times.Count - quantreb)
+                    premiacao = "= Rebaixado";
                 times.Sort((a, b) => b.pont.CompareTo(a.pont));
-                System.Console.WriteLine($"Time: {times[i].time} - Pontuação: {times[i].pont}");
-
-            };
-        }
-            
+                System.Console.WriteLine($"Time: {times[i].time} - Pontuação: {times[i].pont} {premiacao}");
+            };      
+        }            
     }
 }
